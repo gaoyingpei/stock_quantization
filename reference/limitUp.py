577b69fd3ab87496aabcd4d412dc86fdee39ec24
unknown <GAOYINGPEI556@wanlitong.com>
2017-03-22 16:50:03 +0800

    简易量化模型

--- reference/limitUp.py
+++ reference/limitUp.py
@@ -50,6 +50,11 @@ def getLimitUp():
     greenAferGreen = 0 #涨停后第二天收阴第三天收阴
     limitWithoutOneLine = 0 #涨停，非一字板
     limitAfterLimitWithoutOneLine = 0 #连续涨停，非一字板
+    limitupAfterDown = 0 #跌幅超过6%之后涨停次数
+    ladWin = 0 #跌幅超过6%之后涨停之后10天内盈利次数
+    ladLose = 0 #跌幅超过6%之后涨停之后10天内亏损次数
+    rangeWin = 0 #跌幅超过6%之后涨停之后10天内盈利幅度
+    rangeLose = 0 #跌幅超过6%之后涨停之后10天内亏损幅度
 
     # 读取CSV文件获取股票列表
     with open('D:/Python/test/stocklist.csv','r', encoding='utf-8') as csvfile:
@@ -71,33 +76,48 @@ def getLimitUp():
             totalCount += addCnt['total']
             redCount += addCnt['red']
             greenCount += addCnt['green']
+
             redAferRed += addCnt['rAr']
             greenAferRed += addCnt['gAr']
             redAferGreen += addCnt['rAg']
             greenAferGreen += addCnt['gAg']
+
             limitWithoutOneLine += addCnt['limit']
             limitAfterLimitWithoutOneLine += addCnt['lAl']
+
+            limitupAfterDown += addCnt['lAd']
+            ladWin += addCnt['ladWin']
+            ladLose += addCnt['ladLose']
+            rangeWin += addCnt['rWin']
+            rangeLose += addCnt['rLose']
         
     print('******************** start ********************')
-    print('统计股票数：' + str(stockCount))
-    print('涨停总数：' + str(totalCount))
-    print('******************** Day1 *********************')
-    print('涨停=>收阳 总数：' + str(redCount))
-    print('涨停=>收阴 总数：' + str(greenCount))
-    print('涨停=>收阳 概率：' + str(redCount/totalCount))
-    print('涨停=>收阴 概率：' + str(greenCount/totalCount))
-    print('******************** Day2 *********************')
-    print('涨停=>收阳=>收阳 总数：' + str(redAferRed))
-    print('涨停=>收阳=>收阴 总数：' + str(greenAferRed))
-    print('涨停=>收阴=>收阳 总数：' + str(redAferGreen))
-    print('涨停=>收阴=>收阴 总数：' + str(greenAferGreen))
-    print('涨停=>收阳=>收阳 概率：' + str(redAferRed/redCount))
-    print('涨停=>收阳=>收阴 概率：' + str(greenAferRed/redCount))
-    print('涨停=>收阴=>收阳 概率：' + str(redAferGreen/greenCount))
-    print('涨停=>收阴=>收阴 概率：' + str(greenAferGreen/greenCount))
-    print('******************** 连续涨停 *********************')
-    print('涨停 总数：' + str(limitWithoutOneLine))
-    print('涨停=>涨停 概率：' + str(limitAfterLimitWithoutOneLine/limitWithoutOneLine))
+    # print('统计股票数：' + str(stockCount))
+    # print('涨停总数：' + str(totalCount))
+    # print('******************** Day1 *********************')
+    # print('涨停=>收阳 总数：' + str(redCount))
+    # print('涨停=>收阴 总数：' + str(greenCount))
+    # print('涨停=>收阳 概率：' + str(redCount/totalCount))
+    # print('涨停=>收阴 概率：' + str(greenCount/totalCount))
+    # print('******************** Day2 *********************')
+    # print('涨停=>收阳=>收阳 总数：' + str(redAferRed))
+    # print('涨停=>收阳=>收阴 总数：' + str(greenAferRed))
+    # print('涨停=>收阴=>收阳 总数：' + str(redAferGreen))
+    # print('涨停=>收阴=>收阴 总数：' + str(greenAferGreen))
+    # print('涨停=>收阳=>收阳 概率：' + str(redAferRed/redCount))
+    # print('涨停=>收阳=>收阴 概率：' + str(greenAferRed/redCount))
+    # print('涨停=>收阴=>收阳 概率：' + str(redAferGreen/greenCount))
+    # print('涨停=>收阴=>收阴 概率：' + str(greenAferGreen/greenCount))
+    # print('******************** 连续涨停 *********************')
+    # print('涨停 总数：' + str(limitWithoutOneLine))
+    # print('涨停=>涨停 概率：' + str(limitAfterLimitWithoutOneLine/limitWithoutOneLine))
+    print('****** 大跌后涨停价买入10天后收盘价卖出胜率 *******')
+    print('大跌6%=>涨停 总数：' + str(limitupAfterDown))
+    print('大跌6%=>涨停=>10日后收盘价平仓 胜率：' + str(ladWin/limitupAfterDown))
+    print('大跌6%=>涨停=>10日后收盘价平仓 平均升幅：' + str(rangeWin/ladWin))
+    print('大跌6%=>涨停=>10日后收盘价平仓 平均跌幅：' + str(rangeLose/ladLose))
+    print('大跌6%=>涨停=>10日后收盘价平仓 投入100000可盈利：' + str(100000 * (rangeWin/ladWin) * (ladWin/limitupAfterDown) - 100000 * abs(rangeLose/ladLose) * (ladLose/limitupAfterDown)) + '元')
+    # print('大跌6%=>涨停=>10日后收盘价平仓 每次投入100000总盈利：' + str(100000 * (rangeWin/ladWin) * ladWin - 100000 * abs(rangeLose/ladLose) * ladLose) + '元')
     print('******************** end **********************\n')
 
 
@@ -114,6 +134,11 @@ def getOneCount(stock):
     limitWithoutOneLine = 0 #涨停，非一字板
     limitAfterLimitWithoutOneLine = 0 #连续涨停，非一字板
     newMarketDay = 0 #新上市经过日期
+    limitupAfterDown = 0 #跌幅超过6%之后涨停次数
+    ladWin = 0 #跌幅超过6%之后涨停之后10天内盈利次数
+    ladLose = 0 #跌幅超过6%之后涨停之后10天内亏损次数
+    rangeWin = 0 #跌幅超过6%之后涨停之后10天内盈利幅度
+    rangeLose = 0 #跌幅超过6%之后涨停之后10天内亏损幅度
 
     # 未复权数据接口(对应函数get_hist_data)
     for i in range(len(stock)-1, 0, -1):
@@ -126,6 +151,27 @@ def getOneCount(stock):
         if (i >= len(stock) - 5 or stock[i]['open'] == 0 or stock[i + 5]['close'] == 0):
             continue
 
+        # 下跌幅度超过6%
+        if (stock[i + 11]['p_change'] < -6 and stock[i + 10]['p_change'] >= 9.5 and stock[i + 10]['p_change'] < 12):
+            # 排除涨停一字板
+            if stock[i + 9]['low'] != stock[i + 9]['high']:
+                limitupAfterDown += 1
+                buyPrice = stock[i + 9]['open']
+                sellPrice = stock[i]['close']
+                # 排除跌停一字板
+                for j in range(i, -1, -1):
+                    if ((stock[j]['p_change'] > -9.5 and stock[j]['low'] != stock[j]['high']) or j == 0):
+                        sellPrice = stock[j]['close']
+                        break
+
+                buyRange = (sellPrice - buyPrice) / buyPrice - 0.002
+                if buyRange >= 0:
+                    ladWin += 1
+                    rangeWin += buyRange
+                else:
+                    ladLose += 1
+                    rangeLose += buyRange
+
         # 涨停
         limitUpFlg = 1 if (9.5 <= stock[i + 2]['p_change'] and stock[i + 2]['p_change'] < 12) else 0
         if limitUpFlg == 1:
@@ -133,7 +179,8 @@ def getOneCount(stock):
         # 连续涨停，非一字板
         if limitUpFlg == 1 and (stock[i + 2]['close'] - stock[i + 2]['open']) / stock[i + 2]['open'] > 0.01:
             limitWithoutOneLine += 1
-            if (9.5 <= stock[i + 1]['p_change'] and stock[i + 1]['p_change'] < 12) and ((stock[i + 1]['close'] - stock[i + 1]['open']) / stock[i + 1]['open'] > 0.01):
+            if (9.5 <= stock[i + 1]['p_change'] and stock[i + 1]['p_change'] < 12) and\
+                ((stock[i + 1]['close'] - stock[i + 1]['open']) / stock[i + 1]['open'] > 0.01):
                 limitAfterLimitWithoutOneLine += 1
         # 昨天涨停并且当天收阳
         if (stock[i + 1]['p_change'] > 0 and limitUpFlg == 1):
@@ -154,10 +201,13 @@ def getOneCount(stock):
             if (stock[i]['p_change'] < 0):
                 greenAferGreen += 1
 
-    return {'red':redCount, 'green':greenCount, 'total':totalCount, 'rAr':redAferRed, 'gAr':greenAferRed, 'rAg':redAferGreen, 'gAg':greenAferGreen, 'limit':limitWithoutOneLine, 'lAl':limitAfterLimitWithoutOneLine}
+    return {'red':redCount, 'green':greenCount, 'total':totalCount,\
+            'rAr':redAferRed, 'gAr':greenAferRed, 'rAg':redAferGreen,\
+            'gAg':greenAferGreen, 'limit':limitWithoutOneLine, 'lAl':limitAfterLimitWithoutOneLine,\
+            'lAd':limitupAfterDown, 'ladWin':ladWin, 'ladLose':ladLose, 'rWin':rangeWin, 'rLose':rangeLose}
 
 
 if __name__ == '__main__':  
-    #downloadStockList()
-    #downloadStockData()
+    # downloadStockList()
+    # downloadStockData()
     getLimitUp()
