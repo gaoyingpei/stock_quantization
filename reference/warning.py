77dc54d813e2f61a211fbeef26970644b4297f8c
unknown <GAOYINGPEI556@wanlitong.com>
2017-05-02 18:40:14 +0800

    大红字提示

--- reference/warning.py
+++ reference/warning.py
@@ -1,5 +1,7 @@
 import wx  
-  
+
+import matplotlib.pyplot as plt
+from PIL import Image
 # begin wxGlade: extracode  
 # end wxGlade  
   
@@ -9,183 +11,65 @@ class MyFrame(wx.Frame):
         # begin wxGlade: MyFrame.__init__  
         kwds["style"] = wx.DEFAULT_FRAME_STYLE  
         wx.Frame.__init__(self, *args, **kwds)  
-        self.text_ctrl_1 = wx.TextCtrl(self, -1, "",style=wx.TE_READONLY)  
-        self.button_37 = wx.Button(self, -1, "1")  
-        self.button_38 = wx.Button(self, -1, "2")  
-        self.button_39 = wx.Button(self, -1, "3")  
-        self.button_40 = wx.Button(self, -1, "+")  
-        self.button_41 = wx.Button(self, -1, "4")  
-        self.button_42 = wx.Button(self, -1, "5")  
-        self.button_43 = wx.Button(self, -1, "6")  
-        self.button_44 = wx.Button(self, -1, "-")  
-        self.button_46 = wx.Button(self, -1, "7")  
-        self.button_45 = wx.Button(self, -1, "8")  
-        self.button_47 = wx.Button(self, -1, "9")  
-        self.button_48 = wx.Button(self, -1, "x")  
-        self.button_49 = wx.Button(self, -1, "C")  
-        self.button_50 = wx.Button(self, -1, "0")  
-        self.button_51 = wx.Button(self, -1, "=")  
-        self.button_52 = wx.Button(self, -1, "/")  
+        self.totalAmount = wx.TextCtrl(self, -1, "总仓位(万)",style=wx.TE_READONLY)  
+        self.todayPercent = wx.TextCtrl(self, -1, "今天可投入比例(%)",style=wx.TE_READONLY)  
+        self.todayAmount = wx.TextCtrl(self, -1, "今天投入资金(万)",style=wx.TE_READONLY)  
+        self.total = wx.TextCtrl(self, -1, "",style=wx.TE_CENTRE) 
+        self.percent = wx.TextCtrl(self, -1, "",style=wx.TE_CENTRE)  
+        self.amount = wx.TextCtrl(self, -1, "",style=wx.TE_CENTRE)  
+        self.warning = wx.Button(self, -1, "判断预警")  
   
         self.__set_properties()  
         self.__do_layout()  
   
-        self.Bind(wx.EVT_BUTTON, self.bu1, self.button_37)  
-        self.Bind(wx.EVT_BUTTON, self.bu2, self.button_38)  
-        self.Bind(wx.EVT_BUTTON, self.bu3, self.button_39)  
-        self.Bind(wx.EVT_BUTTON, self.bu_plus, self.button_40)  
-        self.Bind(wx.EVT_BUTTON, self.bu4, self.button_41)  
-        self.Bind(wx.EVT_BUTTON, self.bu5, self.button_42)  
-        self.Bind(wx.EVT_BUTTON, self.bu6, self.button_43)  
-        self.Bind(wx.EVT_BUTTON, self.bu_min, self.button_44)  
-        self.Bind(wx.EVT_BUTTON, self.bu7, self.button_46)  
-        self.Bind(wx.EVT_BUTTON, self.bu8, self.button_45)  
-        self.Bind(wx.EVT_BUTTON, self.bu9, self.button_47)  
-        self.Bind(wx.EVT_BUTTON, self.bu_mul, self.button_48)  
-        self.Bind(wx.EVT_BUTTON, self.bu_clear, self.button_49)  
-        self.Bind(wx.EVT_BUTTON, self.bu0, self.button_50)  
-        self.Bind(wx.EVT_BUTTON, self.bu_result, self.button_51)  
-        self.Bind(wx.EVT_BUTTON, self.bu_chu, self.button_52)  
+        self.Bind(wx.EVT_BUTTON, self.warn, self.warning)  
         # end wxGlade  
         self.Show(True)  
   
     def __set_properties(self):  
         # begin wxGlade: MyFrame.__set_properties  
-        self.SetTitle("Python Calculater by CYG")  
-        self.text_ctrl_1.SetMinSize((400, 50))  
-        self.button_37.SetMinSize((100, 50))  
-        self.button_38.SetMinSize((100, 50))  
-        self.button_39.SetMinSize((100, 50))  
-        self.button_40.SetMinSize((100, 50))  
-        self.button_41.SetMinSize((100, 50))  
-        self.button_42.SetMinSize((100, 50))  
-        self.button_43.SetMinSize((100, 50))  
-        self.button_44.SetMinSize((100, 50))  
-        self.button_46.SetMinSize((100, 50))  
-        self.button_45.SetMinSize((100, 50))  
-        self.button_47.SetMinSize((100, 50))  
-        self.button_48.SetMinSize((100, 50))  
-        self.button_49.SetMinSize((100, 50))  
-        self.button_50.SetMinSize((100, 50))  
-        self.button_51.SetMinSize((100, 50))  
-        self.button_52.SetMinSize((100, 50))  
+        self.SetTitle("Python Warning by GYP")  
+        self.totalAmount.SetMinSize((200, 40))  
+        self.total.SetMinSize((200, 40)) 
+        self.todayPercent.SetMinSize((200, 40))  
+        self.percent.SetMinSize((200, 40))  
+        self.todayAmount.SetMinSize((200, 40))  
+        self.amount.SetMinSize((200, 40))  
+        self.warning.SetMinSize((10, 10))  
         # end wxGlade  
   
     def __do_layout(self):  
         # begin wxGlade: MyFrame.__do_layout  
-        sizer_2 = wx.BoxSizer(wx.VERTICAL)  
-        sizer_3 = wx.BoxSizer(wx.VERTICAL)  
-        grid_sizer_1 = wx.GridSizer(4, 4, 0, 0)  
-        sizer_3.Add(self.text_ctrl_1, 0, 0, 0)  
-        grid_sizer_1.Add(self.button_37, 0, 0, 0)  
-        grid_sizer_1.Add(self.button_38, 0, 0, 0)  
-        grid_sizer_1.Add(self.button_39, 0, 0, 0)  
-        grid_sizer_1.Add(self.button_40, 0, 0, 0)  
-        grid_sizer_1.Add(self.button_41, 0, 0, 0)  
-        grid_sizer_1.Add(self.button_42, 0, 0, 0)  
-        grid_sizer_1.Add(self.button_43, 0, 0, 0)  
-        grid_sizer_1.Add(self.button_44, 0, 0, 0)  
-        grid_sizer_1.Add(self.button_46, 0, 0, 0)  
-        grid_sizer_1.Add(self.button_45, 0, 0, 0)  
-        grid_sizer_1.Add(self.button_47, 0, 0, 0)  
-        grid_sizer_1.Add(self.button_48, 0, 0, 0)  
-        grid_sizer_1.Add(self.button_49, 0, 0, 0)  
-        grid_sizer_1.Add(self.button_50, 0, 0, 0)  
-        grid_sizer_1.Add(self.button_51, 0, 0, 0)  
-        grid_sizer_1.Add(self.button_52, 0, 0, 0)  
-        sizer_3.Add(grid_sizer_1, 1, wx.EXPAND, 0)  
-        sizer_2.Add(sizer_3, 1, wx.EXPAND, 0)  
-        self.SetSizer(sizer_2)  
-        sizer_2.Fit(self)  
+        sizer_1 = wx.BoxSizer(wx.VERTICAL)  
+        grid_sizer_1 = wx.GridSizer(3, 2, 0, 0)  
+        grid_sizer_1.Add(self.totalAmount, 0, 0, 0)  
+        grid_sizer_1.Add(self.total, 0, 0, 0)  
+        grid_sizer_1.Add(self.todayPercent, 0, 0, 0)  
+        grid_sizer_1.Add(self.percent, 0, 0, 0)  
+        grid_sizer_1.Add(self.todayAmount, 0, 0, 0)  
+        grid_sizer_1.Add(self.amount, 0, 0, 0)  
+        sizer_1.Add(grid_sizer_1, 1, wx.EXPAND, 0)
+        sizer_1.Add(self.warning, 1, wx.EXPAND, 0)
+        self.SetSizer(sizer_1)  
+        sizer_1.Fit(self)  
         self.Layout()  
         # end wxGlade  
-  
-    def bu1(self, event):  # wxGlade: MyFrame.<event_handler>  
-        self.text_ctrl_1.AppendText("1")  
-        event.Skip()  
-  
-    def bu2(self, event):  # wxGlade: MyFrame.<event_handler>  
-        self.text_ctrl_1.AppendText("2")  
-        event.Skip()  
-  
-    def bu3(self, event):  # wxGlade: MyFrame.<event_handler>  
-        self.text_ctrl_1.AppendText("3")  
-        event.Skip()  
-  
-    def bu_plus(self, event):  # wxGlade: MyFrame.<event_handler>  
-        self.num1=self.text_ctrl_1.GetValue()  
-        self.op="+"  
-        self.text_ctrl_1.Clear()  
-        event.Skip()  
-  
-    def bu4(self, event):  # wxGlade: MyFrame.<event_handler>  
-        self.text_ctrl_1.AppendText("4")  
-        event.Skip()  
-  
-    def bu5(self, event):  # wxGlade: MyFrame.<event_handler>  
-        self.text_ctrl_1.AppendText("5")  
-        event.Skip()  
-  
-    def bu6(self, event):  # wxGlade: MyFrame.<event_handler>  
-        self.text_ctrl_1.AppendText("6")  
-        event.Skip()  
-  
-    def bu_min(self, event):  # wxGlade: MyFrame.<event_handler>  
-        self.num1=self.text_ctrl_1.GetValue()  
-        self.op="-"  
-        self.text_ctrl_1.Clear()  
-        event.Skip()  
-  
-    def bu7(self, event):  # wxGlade: MyFrame.<event_handler>  
-        self.text_ctrl_1.AppendText("7")  
-        event.Skip()  
-  
-    def bu8(self, event):  # wxGlade: MyFrame.<event_handler>  
-        self.text_ctrl_1.AppendText("8")  
-        event.Skip()  
-  
-    def bu9(self, event):  # wxGlade: MyFrame.<event_handler>  
-        self.text_ctrl_1.AppendText("9")  
-        event.Skip()  
-  
-    def bu_mul(self, event):  # wxGlade: MyFrame.<event_handler>  
-        self.num1=self.text_ctrl_1.GetValue()  
-        self.op="x"  
-        self.text_ctrl_1.Clear()  
-        event.Skip()  
-  
-    def bu_clear(self, event):  # wxGlade: MyFrame.<event_handler>  
-        self.text_ctrl_1.Clear()  
-        event.Skip()  
-  
-    def bu0(self, event):  # wxGlade: MyFrame.<event_handler>  
-        self.text_ctrl_1.AppendText("0")  
-        event.Skip()  
-  
-    def bu_chu(self, event):  # wxGlade: MyFrame.<event_handler>  
-        self.num1=self.text_ctrl_1.GetValue()  
-        self.op="/"  
-        self.text_ctrl_1.Clear()  
-        event.Skip()  
-  
-    def bu_result(self, event):  # wxGlade: MyFrame.<event_handler>  
-        self.num2=self.text_ctrl_1.GetValue()  
-        num1=int(self.num1)  
-        num2=int(self.num2)  
-        if self.op=="+":  
-            self.text_ctrl_1.SetValue(str(num1+num2))  
-        elif self.op=="-":  
-            self.text_ctrl_1.SetValue(str(num1-num2))  
-        elif self.op=="x":  
-            self.text_ctrl_1.SetValue(str(num1*num2))  
-        elif self.op=="/":  
-            self.text_ctrl_1.SetValue(str(num1/num2))  
-        event.Skip()  
-  
+
+    def warn(self, event):  # wxGlade: MyFrame.<event_handler>  
+        self.warnFlg = float(self.total.GetValue()) * float(self.percent.GetValue()) - float(self.amount.GetValue())
+
+        if self.warnFlg < 0:
+            img=Image.open('./img/红字.PNG')
+            plt.figure("限额警告", figsize=(30,8))
+            plt.imshow(img)
+            plt.show()
+        else:
+            wx.MessageBox("可继续投入", "允许" ,wx.OK | wx.ICON_INFORMATION)
+
+        event.Skip()
   
 # end of class MyFrame  
 if __name__ == "__main__":  
-  
     app=wx.App(False)  
     myframe=MyFrame(None)  
     app.MainLoop()
