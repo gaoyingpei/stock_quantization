36bd74822440eb0e5514de0a6bfc050e84b19622
unknown <GAOYINGPEI556@wanlitong.com>
2017-05-08 10:04:45 +0800

    追加一字板

--- reference/increase.py
+++ reference/increase.py
@@ -79,6 +79,8 @@ class MyFrame(wx.Frame):
         cntArray = [0 for i in range(22)]
         rangeArray = [9.5, 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0, 0, -1.0, -2.0, -3.0, -4.0, -5.0, -6.0, -7.0, -8.0, -9.0, -9.5]
         total = 0
+        oneLimitUp = 0
+        oneLimitDown = 0
         # 读取CSV文件获取股票列表
         with open(self.path + 'stockList.csv', 'r', encoding='utf-8') as f:
             stockList = csv.reader(f)
@@ -102,18 +104,24 @@ class MyFrame(wx.Frame):
                 # 判断涨幅范围
                 increase = float('%.2f' % stock.p_change[0])
                 for i in range(0, len(rangeArray), 1):
-                    if (i == 0 and increase >= rangeArray[i]):
+                    if (increase >= rangeArray[0] and stock.open == stock.close):
+                        oneLimitUp += 1
+                    elif (increase <= rangeArray[20] and stock.open == stock.close):
+                        oneLimitDown += 1
+                    elif (i == 0 and increase >= rangeArray[i]):
                         cntArray[i] += 1
                     elif (i == len(rangeArray)-1 and increase < rangeArray[-1]):
                         cntArray[i+1] += 1
                     elif (i > 0 and i < len(rangeArray)-1 and increase >= rangeArray[i] and increase < rangeArray[i-1]):
                         cntArray[i] += 1
 
+        cntArray.insert(0, oneLimitDown)
+        cntArray.insert(0, oneLimitUp)
         cntArray.insert(0, total)
         # 将今日涨幅统计追加到每日统计列表中
-        todayData = DataFrame(np.array(cntArray).reshape(1, 23),\
+        todayData = DataFrame(np.array(cntArray).reshape(1, 25),\
                                 index=[startTime],\
-                                columns=['total','[9.5～]','[9.0～9.5)','[8.0～9.0)','[7.0～8.0)','[6.0～7.0)',\
+                                columns=['合计','一字涨停','一字跌停','[9.5～]','[9.0～9.5)','[8.0～9.0)','[7.0～8.0)','[6.0～7.0)',\
                                             '[5.0～6.0)','[4.0～5.0)','[3.0～4.0)','[2.0～3.0)','[1.0～2.0)',\
                                             '[0～1.0)','[-1.0～0)','[-2.0～-1.0)','[-3.0～-2.0)','[-4.0～-3.0)','[-5.0～-4.0)',\
                                             '[-6.0～-5.0)','[-7.0～-6.0)','[-8.0～-7.0)','[-9.0～-8.0)','[-9.5～-9.0)','[～-9.5)'])
